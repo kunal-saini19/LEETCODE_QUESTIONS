47136ebab11756030c2dbf149cf6b1736b36892c
kunal-saini19 <kunal876saini@gmail.com>
2026-03-04 22:32:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (80.53%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -4,11 +4,15 @@ public:
         vector<int> nums;
         int jai=0;
 
-        for(int i = 0 ; i < n ; i++ ){
-            nums.push_back(start+2*i);
-        }
-        for(int o:nums){
-            jai^=o;
+        // for(int i = 0 ; i < n ; i++ ){
+        //     nums.push_back(start+2*i);
+        // }
+        // for(int o:nums){
+        //     jai^=o;
+        // }
+        // return jai;
+        for(int i=0; i < n;i++){
+            jai^=start+2*i;
         }
         return jai;
     }
