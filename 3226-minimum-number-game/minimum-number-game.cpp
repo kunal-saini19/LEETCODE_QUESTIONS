6d89aa4288ae00d48b43bd45c8429b20b65ab43f
kunal-saini19 <kunal876saini@gmail.com>
2026-03-06 06:08:29 +0530

    Time: 1 ms (33.19%) | Memory: 28.5 MB (19.91%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     vector<int> numberGame(vector<int>& nums) {
-        vector<int>ans;
-        if(nums.size()!=0){
-            while(nums.size()>0){
-                int a=*min_element(nums.begin(),nums.end());
-                int alice=a;
-                auto it =find(nums.begin(),nums.end(),a);
-                if(it != nums.end()){
-                    nums.erase(it);
-                }
-                int b=*min_element(nums.begin(),nums.end());
-                int bob=b;
-                auto t =find(nums.begin(),nums.end(),b);
-                if(t != nums.end()){
-                    nums.erase(t);
-                }
-                ans.push_back(bob);
-                ans.push_back(alice);
-            }
+        // vector<int>ans;
+        // if(nums.size()!=0){
+        //     while(nums.size()>0){
+        //         int a=*min_element(nums.begin(),nums.end());
+        //         int alice=a;
+        //         auto it =find(nums.begin(),nums.end(),a);
+        //         if(it != nums.end()){
+        //             nums.erase(it);
+        //         }
+        //         int b=*min_element(nums.begin(),nums.end());
+        //         int bob=b;
+        //         auto t =find(nums.begin(),nums.end(),b);
+        //         if(t != nums.end()){
+        //             nums.erase(t);
+        //         }
+        //         ans.push_back(bob);
+        //         ans.push_back(alice);
+        //     }
+        // }
+        // return ans;
+
+    // complexity O(nlogn )
+           sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        for(int i = 0; i < nums.size(); i += 2){
+            ans.push_back(nums[i+1]);
+            ans.push_back(nums[i]);
         }
+
         return ans;
     }
 };
