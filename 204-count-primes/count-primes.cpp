69f89f6051b7458077939c110ef738e6c5b87fc8
kunal-saini19 <kunal876saini@gmail.com>
2026-03-28 22:44:56 +0530

    Time: 241 ms (19.26%) | Memory: 12.8 MB (37.30%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>prime(n+1,true);
+        int count=0;
+        for(int i =2;i<n;i++){
+            if(prime[i]){
+                count++;
+                for(int j=i*2;j<n;j=j+i){
+                    prime[j]=false;
+                }
+            }
+        }
+        return count;
+    }
+};
