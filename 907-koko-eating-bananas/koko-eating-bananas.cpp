eff19a5b6f23236ff58674367abe208ece773a31
kunal-saini19 <kunal876saini@gmail.com>
2026-05-30 23:25:49 +0530

    Time: 5 ms (79.09%) | Memory: 22.9 MB (77.64%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,63 +1,29 @@
 class Solution {
 public:
-    bool cutieeKOKO(vector<int> &piles,int target,int maxallowedtime ){
-        long long  a=0;
-        for(int banana:piles){
-            a+=(banana+target-1)/target;
+    bool isvalid(vector<int>&piles,int maximumspeed,int h){
+        // int banana=0;
+        long long speed=0;;
+        for(int i=0;i<piles.size();i++){
+            speed+=(maximumspeed+piles[i]-1)/maximumspeed;
         }
-      return a<=maxallowedtime;
+        return speed<=h;
     }
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int n=piles.size();
-        int start =1;
-        int end = *max_element(piles.begin(),piles.end());
-        // for(int a:piles){
-        //     end+=a;
-        // }
         
-        int ans=0;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(cutieeKOKO(piles,mid,h)){
-                ans=mid;
-                end=mid-1;
+        int minEatingSpeed(vector<int>& piles, int h) {
+            if(h<piles.size()-1) return -1;
+            int start=1;
+            int ans=0;
+            int last=*max_element(piles.begin(),piles.end());
+            while(start<=last){
+                int mid=start+(last-start)/2;
+                if(isvalid(piles,mid,h)){
+                    ans=mid;
+                    last=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
             }
-            else{
-                start=mid+1;
-            }
-        }
-        return ans;
+            return ans;
     }
 };
-// class Solution {
-// public:
-
-//     bool canFinish(vector<int>& piles, int speed, int h) {
-//         long long time = 0;
-
-//         for (int bananas : piles) {
-//             time += (bananas + speed - 1) / speed; // ceil division
-//         }
-
-//         return time <= h;
-//     }
-
-//     int minEatingSpeed(vector<int>& piles, int h) {
-//         int start = 1;
-//         int end = *max_element(piles.begin(), piles.end());
-//         int ans = end;
-
-//         while (start <= end) {
-//             int mid = start + (end - start) / 2;
-
-//             if (canFinish(piles, mid, h)) {
-//                 ans = mid;
-//                 end = mid - 1;   // try smaller speed
-//             } else {
-//                 start = mid + 1; // need faster speed
-//             }
-//         }
-
-//         return ans;
-//     }
-// };
