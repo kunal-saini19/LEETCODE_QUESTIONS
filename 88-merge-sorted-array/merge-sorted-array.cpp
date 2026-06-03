5d0a2604687d8642b01c4b715bcb1ac20401250c
kunal-saini19 <kunal876saini@gmail.com>
2026-06-03 07:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (96.53%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,64 +1,21 @@
 class Solution {
 public:
-    void merging (vector<int>&nums,int start,int mid,int end){
-        int i=start;
-        int j=mid+1;
-        vector<int>temp;
-        while(i<=mid && j<=end){
-            if(nums[i]<=nums[j]){
-                temp.push_back(nums[i]);
-                i++;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int inx=m+n-1;
+        int i=m-1;
+        int j=n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[inx--]=nums1[i--];
             }
             else{
-                temp.push_back(nums[j]);
-                j++;
+                nums1[inx--]=nums2[j--];
             }
+             
         }
-        while(i<=mid){
-            temp.push_back(nums[i]);
-            i++;
+        while(j>=0){
+            nums1[inx--]=nums2[j--];
         }
-        while(j<=end){
-            temp.push_back(nums[j]);
-            j++;
-        }
-        for (int i = start; i <= end; i++) {
-            nums[i] = temp[i - start];
-        }
-    }
-    void mergesort(vector<int>&nums,int start,int end){
-        // int start=0;
-        // int end=nums.size()-1;
-        if(start>=end) return ;
-        int mid=start+(end-start)/2;
-        mergesort(nums,start,mid);
-        mergesort(nums,mid+1,end);
-        merging(nums,start,mid,end);
-        
-    }
-    // void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    //     for(int i=0;i<n;i++){
-    //         nums1.push_back(nums2[i]);
-           
-    //     }
-    //     vector<int>h;
-    //     int k=nums1.size();
-    //     mergesort(nums1,0,k-1);
-    //     for(int i=0;i<k;i++){
-    //         if(nums1[i] !=0){
-    //             h.push_back(nums1[i]);
-    //         }
-    //     }
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // FIX 1: Overwrite the placeholder zeros in nums1 with nums2 elements
-        // nums1 already has size m + n. Don't use push_back.
-        for (int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
-        }
-
-        // FIX 2: Sort the entire combined array (size is m + n)
-        mergesort(nums1, 0, m + n - 1);
-      
-
+       
     }
 };
