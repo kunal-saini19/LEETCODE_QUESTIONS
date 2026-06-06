80d32cf5dc8d8938912f55f2cca1935ac02c6766
kunal-saini19 <kunal876saini@gmail.com>
2026-06-06 15:06:36 +0530

    Time: 79 ms (23.26%) | Memory: 21.2 MB (13.28%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,42 +1,30 @@
-
-   class Solution {
+class Solution {
 public:
-
-    bool isfresame(vector<int>& freq1, vector<int>& freq2){
-        for(int i = 0; i < 26; i++){
+    bool isfreqsame(vector<int> &freq1,vector<int> &freq2){
+        for(int i=0;i<26;i++ ){
             if(freq1[i] != freq2[i]){
-                return false;
+                return false;                
             }
         }
         return true;
     }
-
     bool checkInclusion(string s1, string s2) {
-
         if (s1.length() > s2.length()) return false;
 
-        vector<int> freq1(26, 0);
-        for(char a : s1){
-            freq1[a - 'a']++;
+        vector<int>freq1(26,0);
+        for(int i=0;i<s1.length();i++){
+            freq1[s1[i]-'a']++;
         }
-
-        int windowSize = s1.length();
-
-        // sliding window
-        for(int i = 0; i + windowSize <= s2.length(); i++){
-
-            vector<int> freq2(26, 0);
-
-            // build window frequency correctly
-            for(int j = i; j < i + windowSize; j++){
-                freq2[s2[j] - 'a']++;
+        int windowsize=s1.length();
+        for(int  i = 0; i+windowsize <=s2.length();i++){
+            vector<int>freq2(26,0);
+            for(int j=i; j <i+windowsize;j++){
+                freq2[s2[j]-'a']++;
             }
-
-            if(isfresame(freq1, freq2)){
+            if(isfreqsame(freq1,freq2)){
                 return true;
             }
         }
-
         return false;
     }
 };
