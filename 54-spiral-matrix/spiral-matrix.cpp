0fee71780720595a53ff5a871e915853718b9a60
kunal-saini19 <kunal876saini@gmail.com>
2026-06-10 11:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (80.05%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,58 +1,35 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-          vector<int> ans;
-
-        int top = 0;
-        int bottom = matrix.size() - 1;
-        int left = 0;
-        int right = matrix[0].size() - 1;
-
-        while (top <= bottom && left <= right) {
-
-            // 1. left → right
-            for (int i = left; i <= right; i++) {
+        int left=0;
+        int right=matrix[0].size()-1;
+        int top=0;
+        int bottom=matrix.size()-1;
+        vector<int>ans;
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
                 ans.push_back(matrix[top][i]);
             }
             top++;
 
-            // 2. top → bottom
-            for (int i = top; i <= bottom; i++) {
+            for(int i=top;i<=bottom;i++){
                 ans.push_back(matrix[i][right]);
             }
             right--;
-
-            // 3. right → left
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    ans.push_back(matrix[bottom][i]);
+            if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                ans.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
-
-            // 4. bottom → top
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    ans.push_back(matrix[i][left]);
-                }
+            if(left<=right){
+            for(int i=bottom;i>=top;i-- ){
+                ans.push_back(matrix[i][left]);
+            }
                 left++;
             }
-        }
 
+        }
         return ans;
     }
 };
-const size_t BUFFER_SIZE = 0x6fafffff;
-alignas(std::max_align_t) char buffer[BUFFER_SIZE];
-size_t buffer_pos = 0;
-void* operator new(size_t size) {
-	constexpr std::size_t alignment = alignof(std::max_align_t);
-	size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
-	size_t total_size = size + padding;
-	char* aligned_ptr = &buffer[buffer_pos + padding];
-	buffer_pos += total_size;
-	return aligned_ptr;
-}
-void operator delete(void* ptr, unsigned long) {}
-void operator delete(void* ptr) {}
-void operator delete[](void* ptr) {}
