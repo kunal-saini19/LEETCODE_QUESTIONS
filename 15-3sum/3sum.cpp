ae240b5704b6aac571bed4c845278c1768cbf007
kunal-saini19 <kunal876saini@gmail.com>
2026-06-15 17:34:14 +0530

    Time: 48 ms (53.94%) | Memory: 29.1 MB (72.52%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>>ans;
        
         sort(nums.begin(),nums.end());
-       for(int i=0;i<nums.size()-1;i++){
+       for(int i=0;i<nums.size()-2;i++){
         int k=i+1;
         int j=nums.size()-1;
         if (i > 0 && nums[i] == nums[i - 1]) continue;
