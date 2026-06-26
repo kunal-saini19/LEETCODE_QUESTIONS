bbe8c63e7e7bf4cc0bbe5b58ea2cde1803c2c631
kunal-saini19 <kunal876saini@gmail.com>
2026-06-26 19:33:39 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (5.07%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        // count=0;
+        vector<int>ans;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]!=0){
+                ans.push_back(nums[i]);
+            }
+
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)  
+            ans.push_back(0);
+        }
+        for(int i=0;i<nums.size();i++){
+            nums[i]=ans[i];
+        }        
+
+
+    }
+    
+};
