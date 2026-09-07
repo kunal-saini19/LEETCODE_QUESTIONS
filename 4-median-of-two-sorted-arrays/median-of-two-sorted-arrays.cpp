b98b5ed06436a5255a90caefb41603d908ae8da4
kunal-saini19 <kunal876saini@gmail.com>
2026-09-07 13:15:34 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (79.75%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,56 +1,44 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1,
-                                  vector<int>& nums2) {
+    double findMedianSortedArrays(vector<int>& nums1,vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
 
-        // Binary search smaller array
-        if(nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+        int i=0;
+        int j=0;
+         
+        int prev=0;
+        int curr=0;
 
-        int m = nums1.size();
-        int n = nums2.size();
+        int total=m+n;
 
-        int low = 0;
-        int high = m;
-
-        int half = (m + n + 1) / 2;
-
-        while(low <= high) {
-
-            int i = low + (high - low) / 2;
-            int j = half - i;
-
-            int Aleft  = (i == 0) ? INT_MIN : nums1[i - 1];
-            int Aright = (i == m) ? INT_MAX : nums1[i];
-
-            int Bleft  = (j == 0) ? INT_MIN : nums2[j - 1];
-            int Bright = (j == n) ? INT_MAX : nums2[j];
-
-            // Correct partition
-            if(Aleft <= Bright && Bleft <= Aright) {
-
-                // Odd total
-                if((m + n) % 2 == 1) {
-                    return max(Aleft, Bleft);
+        for(int count=0;count<=total/2;count++){
+            prev=curr;
+            if(i<n && j<m){
+                if(nums1[i]>nums2[j]){
+                    curr=nums2[j];
+                    j++;
                 }
-
-                // Even total
-                return (max(Aleft, Bleft) +
-                        min(Aright, Bright)) / 2.0;
+                else{
+                    curr=nums1[i];
+                    i++;
+                }
             }
-
-            // A has too many elements on left
-            else if(Aleft > Bright) {
-                high = i - 1;
+            else if(i<n){
+                curr=nums1[i];
+                i++;
             }
-
-            // A has too few elements on left
-            else {
-                low = i + 1;
+            else{
+                curr=nums2[j];
+                j++;
             }
         }
+        if(total%2==1){
+            return curr;
+        }
+        else{
+            return (prev+curr)/2.0;
+        }
 
-        return 0.0;
     }
 };
