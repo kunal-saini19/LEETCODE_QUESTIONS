e49f2ba341d5cb0edb9a9feee30fa0ffb40abb79
kunal-saini19 <kunal876saini@gmail.com>
2026-09-08 18:03:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (5.52%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -3,14 +3,14 @@ public:
     int countCommas(int n) {
         int len=to_string(n).size();
 
-        if(n<4)return 0;
+        if(len<4)return 0;
         // int a=(len-1)/3;
-        int store=n-1000;
-        if(store<0){
+        
+        if(n-1000<0){
             return 0;
         }
         else{
-            return store+1;
+            return n-1000+1;
         }
         return 0;
     }
