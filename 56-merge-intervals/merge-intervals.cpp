60827bad3343160a6085b423f96229758f21f782
kunal-saini19 <kunal876saini@gmail.com>
2026-09-08 19:52:37 +0530

    Time: 4 ms (66.57%) | Memory: 24.5 MB (19.74%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -15,6 +15,7 @@ public:
         for(int i=1;i<temp.size();i++){
             if(end>=temp[i].first){
                 end=max(end,temp[i].second);
+                continue;
             }
             else{
                 ans.push_back({start,end});
